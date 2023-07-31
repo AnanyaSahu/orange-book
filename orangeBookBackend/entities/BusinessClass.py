67c47d242697e4069f44c2d7f025cf3580b78191
ananya <ananya.sahu0894@gmail.com>
2023-07-31 11:26:28 +0100

    changes for business row transform

--- orangeBookBackend/entities/BusinessClass.py
+++ orangeBookBackend/entities/BusinessClass.py
@@ -1,15 +1,16 @@
 class BusinessObj:
    
-   def __init__(self, businessId, businessName, contactNumber, email, address, ratings, sreviceType, serviceCost,location):
+   def __init__(self, businessId, businessName, contactNumber, email, address,location, ratings, sreviceType, serviceCost):
     self.businessId = businessId
     self.businessName = businessName
     self.contactNumber = contactNumber
     self.email = email
     self.address = address
+    self.location =location
     self.ratings = ratings
     self.sreviceType = sreviceType
     self.serviceCost = serviceCost
-    self.location =location
+
         
             
 
